63e5f34c9fcf41e1204211775cd592107d5a178f
Jovanne234 <jortizreynoso@students.mchenry.edu>
2025-11-03 14:15:52 -0600

    Add files via upload

---
+++ add103/Grid/change.js
@@ -0,0 +1,19 @@
+function change01() {
+    document.getElementById("large").src = "images/food1.JPG";
+}
+
+function change02() {
+    document.getElementById("large").src = "images/food2.JPG";
+}
+
+function change03() {
+    document.getElementById("large").src = "images/food3.JPG";
+}
+
+function change04() {
+    document.getElementById("large").src = "images/food4.JPG";
+}
+
+function change05() {
+    document.getElementById("large").src = "images/food5.JPG";
+}
